9af34db7c7a2110560223226ca1170b2c8bdf546
ponycoder <571533442@qq.com>
2016-05-14 00:09:15 +0800

    framework cocoapod化

---
+++ Pods/PonyFrameworkOnSwift/PonyFrameworkOnSwift/Extensions/Array+/Array+Add.swift
@@ -0,0 +1,25 @@
+//
+//  Array+Add.swift
+//  WowFramework
+//
+//  Created by 小黑 on 16/4/27.
+//  Copyright © 2016年 王云鹏. All rights reserved.
+//
+
+import Foundation
+
+//public extension Array{
+//    mutating func removeObject(object:AnyObject){
+//        if let index = self.indexOf({$0 == object}){
+//            self.removeAtIndex(index)
+//        }
+//    }
+//    
+//    
+//    mutating func removeObjectsInArray(objects:[Element]){
+//        for object in objects {
+//            self.removeObject(object)
+//        }
+//    }
+//    
+//}

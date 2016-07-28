68b5f87e8ded39a055c924e699a763d01feb4f74
ayc <anyongchao@anyongchaodeMacBook-Pro.local>
2016-07-28 18:02:38 +0800

    pod regenerater

---
+++ Pods/Target Support Files/QiniuTokenIOS/QiniuTokenIOS-umbrella.h
@@ -0,0 +1,7 @@
+#import <UIKit/UIKit.h>
+
+#import "GCQiniuUploadManager.h"
+
+FOUNDATION_EXPORT double QiniuTokenIOSVersionNumber;
+FOUNDATION_EXPORT const unsigned char QiniuTokenIOSVersionString[];
+

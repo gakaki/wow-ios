738ba971f7e872a3a0615abef37fc4f6d0ce02d0
陈旭 <chenxu@chenxudeMacBook-Pro.local>
2016-08-08 18:35:06 +0800

    构建订单详情页面

---
+++ wowapp/Classes/User/WOWOrderDetailFourCell.swift
@@ -0,0 +1,24 @@
+//
+//  WOWOrderDetailFourCell.swift
+//  wowapp
+//
+//  Created by 陈旭 on 16/8/8.
+//  Copyright © 2016年 小黑. All rights reserved.
+//
+
+import UIKit
+
+class WOWOrderDetailFourCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}

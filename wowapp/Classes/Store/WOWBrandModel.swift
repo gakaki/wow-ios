30b2a5d2e83c39a77981add9be32072f15fb9c71
ayc <anyongchao@anyongchaodeMacBook-Pro.local>
2016-08-08 18:42:22 +0800

    我喜欢，品牌，，，设计师功能ok

--- wowapp/Classes/Store/WOWBrandModel.swift
+++ wowapp/Classes/Store/WOWBrandModel.swift
@@ -51,7 +51,7 @@ class WOWBrandV1Model: WOWBaseModel,Mappable {
     }
     
     func mapping(map: Map) {
-        id       <- (map["id"],StringDecimalNumberTransform())
+        id       <- map["id"]
         letter   <- map["brandNameFirstLetter"]
         name     <- map["brandCname"]
         image    <- map["brandLogoImg"]

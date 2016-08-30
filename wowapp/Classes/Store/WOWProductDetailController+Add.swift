7e09fe5cf4ea990269875362fec261303afeafef
ayc <anyongchao@anyongchaodeMacBook-Pro.local>
2016-08-30 19:07:35 +0800

    添加XZFresh

--- wowapp/Classes/Store/WOWProductDetailController+Add.swift
+++ wowapp/Classes/Store/WOWProductDetailController+Add.swift
@@ -15,16 +15,20 @@ extension WOWProductDetailController:UITableViewDelegate,UITableViewDataSource{
         tableView.estimatedRowHeight = 200
         tableView.rowHeight          = UITableViewAutomaticDimension
         tableView.mj_header = self.mj_header
-        tableView.tableHeaderView = cycleView
+        tableView.tableHeaderView = cycleView   //banner轮播
+        //显示价格的cell
         tableView.registerNib(UINib.nibName(String(WOWProductDetailPriceCell)), forCellReuseIdentifier:String(WOWProductDetailPriceCell))
+        //品牌故事
         tableView.registerNib(UINib.nibName(String(WOWProductDetailDescCell)), forCellReuseIdentifier:String(WOWProductDetailDescCell))
+        //产品描述
         tableView.registerNib(UINib.nibName(String(WOWProductDetailCell)), forCellReuseIdentifier:String(WOWProductDetailCell))
+        //产品参数
         tableView.registerNib(UINib.nibName(String(WOWProductParameter)), forCellReuseIdentifier:String(WOWProductParameter))
-
+        //温馨提示
         tableView.registerNib(UINib.nibName(String(WOWProductDetailTipsWebViewCell)), forCellReuseIdentifier:String(WOWProductDetailTipsWebViewCell))
-
+        //客服电话
         tableView.registerNib(UINib.nibName(String(WOWTelCell)), forCellReuseIdentifier:String(WOWTelCell))
-        //相关商品也不要
+        //相关商品
         tableView.registerNib(UINib.nibName(String(WOWProductDetailAboutCell)), forCellReuseIdentifier:String(WOWProductDetailAboutCell))
     }
     
@@ -37,13 +41,13 @@ extension WOWProductDetailController:UITableViewDelegate,UITableViewDataSource{
         switch section {
         case 2:
             return 1
-        case 3:
+        case 3: //产品参数可展开
             if isOpenParam {
                 return 1
             }else {
                 return 0   
             }
-        case 4:
+        case 4: //温馨提示也可展开
             if isOpenTips {
                 return 1
             }else {
@@ -78,7 +82,7 @@ extension WOWProductDetailController:UITableViewDelegate,UITableViewDataSource{
             let cell =  tableView.dequeueReusableCellWithIdentifier(String(WOWProductDetailDescCell), forIndexPath: indexPath) as! WOWProductDetailDescCell
             cell.showData(self.productModel)
             returnCell = cell
-        case (2,_):
+        case (2,_): //产品描述
             let cell =  tableView.dequeueReusableCellWithIdentifier(String(WOWProductDetailCell), forIndexPath: indexPath) as! WOWProductDetailCell
             
             returnCell = cell
@@ -101,7 +105,8 @@ extension WOWProductDetailController:UITableViewDelegate,UITableViewDataSource{
             returnCell = cell
         case (6,_)://相关商品
             let cell = tableView.dequeueReusableCellWithIdentifier("WOWProductDetailAboutCell", forIndexPath: indexPath) as! WOWProductDetailAboutCell
-
+            cell.delegate = self
+            cell.showData()
             returnCell = cell
         default:
             break
@@ -112,8 +117,10 @@ extension WOWProductDetailController:UITableViewDelegate,UITableViewDataSource{
     func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         switch section {
         case 2:
-            return 50 + productDescView.productDescLabel.getEstimatedHeight()
+            //动态获取产品描述的label高度
+            return 64 + productDescView.productDescLabel.getEstimatedHeight()
         case 3,4:
+            //产品参数与温馨提示cell头
             return 60
         case 6:
             return 15
@@ -143,9 +150,9 @@ extension WOWProductDetailController:UITableViewDelegate,UITableViewDataSource{
     
     func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         switch section {
-        case 2:
+        case 2:     //产品描述
             return productDescView
-        case 3:
+        case 3:     
             paramView.label.text = "产品参数"
             paramView.openImg.addTapGesture(action: { [weak self](tap) in
                 if let strongSelf = self  {
@@ -192,6 +199,50 @@ extension WOWProductDetailController:UITableViewDelegate,UITableViewDataSource{
     
 }
 
+extension WOWProductDetailController: WOWProductDetailAboutCellDelegate {
+    func requestAboutProduct(productDetailAboutCell:WOWProductDetailAboutCell, pageIndex: Int, isRreshing: Bool, pageSize: Int) {
+        WOWNetManager.sharedManager.requestWithTarget(RequestApi.Api_ProductBrand(brandId:productModel?.brandId ?? 0, pageSize: pageSize, currentPage: pageIndex), successClosure: {(result) in
+            
+                productDetailAboutCell.endRefresh()
+                
+                let arr = Mapper<WOWProductModel>().mapArray(JSON(result)["productVoList"].arrayObject)
+                
+                if let array = arr{
+                    
+                    if productDetailAboutCell.pageIndex == 1{
+                        productDetailAboutCell.dataArr = []
+                    }
+                    productDetailAboutCell.dataArr?.appendContentsOf(array)
+                    //如果请求的数据条数小于totalPage，说明没有数据了，隐藏mj_footer
+                    if array.count < productDetailAboutCell.pageSize {
+                        productDetailAboutCell.collectionView.mj_footer = nil
+                        
+                    }else {
+                        productDetailAboutCell.collectionView.mj_footer = productDetailAboutCell.mj_footer
+                    }
+                    
+                }else {
+                    if productDetailAboutCell.pageIndex == 1{
+                        productDetailAboutCell.dataArr = []
+                    }
+                    
+                    productDetailAboutCell.collectionView.mj_footer = nil
+                    
+                }
+                productDetailAboutCell.collectionView.reloadData()
+                
+            
+            
+        }) {(errorMsg) in
+            
+                productDetailAboutCell.collectionView.mj_footer = nil
+                productDetailAboutCell.endRefresh()
+            
+        }
+        
+
+    }
+}
 
 class DetailSectionFooterView:UIView{
     var containerView = UIView()

1f4cac16cf2494253202a6aeb73dbeaa54025724
陈旭 <897939354@qq.com>
2016-10-21 12:03:03 +0800

    修复首页潜在bug

--- wowdsgn/Classes/Other/BaseController/WOWBaseViewController.swift
+++ wowdsgn/Classes/Other/BaseController/WOWBaseViewController.swift
@@ -8,7 +8,25 @@
 
 import UIKit
 
+public func < <T : Comparable>(lhs: T?, rhs: T?) -> Bool {
+    switch (lhs, rhs) {
+    case let (l?, r?):
+        return l < r
+    case (nil, _?):
+        return true
+    default:
+        return false
+    }
+}
 
+public func > <T : Comparable>(lhs: T?, rhs: T?) -> Bool {
+    switch (lhs, rhs) {
+    case let (l?, r?):
+        return l > r
+    default:
+        return rhs < lhs
+    }
+}
 extension UIViewController {
     static var identifier: String {
         let mirror = Mirror(reflecting: self)
--- wowdsgn/Expand/ExtensionEquatable.swift
+++ wowdsgn/Expand/ExtensionEquatable.swift
@@ -1,22 +1,22 @@
 
 
 import Foundation
-public func < <T: Comparable>(lhs: T?, rhs: T?) -> Bool {
-    switch (lhs, rhs) {
-    case let (l?, r?):
-        return l < r
-    case (nil, _?):
-        return true
-    default:
-        return false
-    }
-}
-
-public func >= <T: Comparable>(lhs: T?, rhs: T?) -> Bool {
-    switch (lhs, rhs) {
-    case let (l?, r?):
-        return l >= r
-    default:
-        return !(lhs < rhs)
-    }
-}
+//public func < <T: Comparable>(lhs: T?, rhs: T?) -> Bool {
+//    switch (lhs, rhs) {
+//    case let (l?, r?):
+//        return l < r
+//    case (nil, _?):
+//        return true
+//    default:
+//        return false
+//    }
+//}
+//
+//public func >= <T: Comparable>(lhs: T?, rhs: T?) -> Bool {
+//    switch (lhs, rhs) {
+//    case let (l?, r?):
+//        return l >= r
+//    default:
+//        return !(lhs < rhs)
+//    }
+//}

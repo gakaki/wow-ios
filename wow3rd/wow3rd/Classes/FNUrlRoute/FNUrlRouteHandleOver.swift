8ca5fdca26f92804b56d5916bd5430ffdda934d2
gakaki <gakaki@gmail.com>
2016-11-07 20:54:17 +0800

    增加router

--- wow3rd/wow3rd/Classes/FNUrlRoute/FNUrlRouteHandleOver.swift
+++ wow3rd/wow3rd/Classes/FNUrlRoute/FNUrlRouteHandleOver.swift
@@ -10,13 +10,16 @@ import SafariServices
 
 //在 key 都没有匹配上的时候，处理 url 的 block
 var urlRouteHandleOverBlock = { (url: String?, modal: Bool?, params:Dictionary<String, AnyClass>?) in
-    let safari = SFSafariViewController.init(url: URL.init(string: url!)!)
-    let topViewController = FNUtil.currentTopViewController()
-    if (topViewController.navigationController != nil) && !modal! {
-        let navigation = topViewController.navigationController
-        navigation?.pushViewController(safari, animated: true)
-    }
-    else {
-        topViewController.present(safari, animated: true, completion: nil)
+    if #available(iOS 9.0, *) {
+        let safari = SFSafariViewController.init(url: URL.init(string: url!)!)
+    
+        let topViewController = FNUtil.currentTopViewController()
+        if (topViewController.navigationController != nil) && !modal! {
+            let navigation = topViewController.navigationController
+            navigation?.pushViewController(safari, animated: true)
+        }
+        else {
+            topViewController.present(safari, animated: true, completion: nil)
+        }
     }
 }

f558834452d50caa48ebd07b499a80b9f0bcddeb
gakaki <gakaki@gmail.com>
2016-11-07 20:18:33 +0800

    增加router组件

---
+++ wow3rd/wow3rd/Classes/RouterRule.swift
@@ -0,0 +1,13 @@
+
+
+import UIKit
+
+class RouterRule {
+    
+    public class func router_init() {
+        FN.open(url: "wowdsgn://m.wowdsgn.com/goodDetail?goodId=123")
+    }
+    
+    
+    
+}

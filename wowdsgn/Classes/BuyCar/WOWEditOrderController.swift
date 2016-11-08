f5c526d65ba4851bdd0da0fcd65b932a53b9e3b1
ayc <anyongchao@anyongchaodeMacBook-Pro.local>
2016-11-08 20:49:08 +0800

    修改细节

--- wowdsgn/Classes/BuyCar/WOWEditOrderController.swift
+++ wowdsgn/Classes/BuyCar/WOWEditOrderController.swift
@@ -603,7 +603,7 @@ extension WOWEditOrderController:UITableViewDelegate,UITableViewDataSource,UITex
 //MARK: - selectPayDelegate
 extension WOWEditOrderController: selectPayDelegate {
     func surePay(_ channel: String) {
-//        backView.hidePayView()
+        backView.hidePayView()
         if  orderCode.isEmpty {
             WOWHud.showMsg("订单生成失败")
             return

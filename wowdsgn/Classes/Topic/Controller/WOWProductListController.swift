c0f958eab33e72a9c03f1f2d03618f39c7422afb
陈旭 <897939354@qq.com>
2016-12-16 18:41:26 +0800

    最新最热产品组列表页

---
+++ wowdsgn/Classes/Topic/Controller/WOWProductListController.swift
@@ -0,0 +1,35 @@
+//
+//  WOWProductListController.swift
+//  wowdsgn
+//
+//  Created by 陈旭 on 2016/12/16.
+//  Copyright © 2016年 g. All rights reserved.
+//
+
+import UIKit
+
+class WOWProductListController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

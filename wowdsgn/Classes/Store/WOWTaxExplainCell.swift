ce652997b28635b03a22642d1cdde0d35abe6b27
ayc <anyongchao@anyongchaodembp.wow.lc>
2017-05-16 11:19:50 +0800

    增加海外购标识

---
+++ wowdsgn/Classes/Store/WOWTaxExplainCell.swift
@@ -0,0 +1,24 @@
+//
+//  WOWTaxExplainCell.swift
+//  wowdsgn
+//
+//  Created by 安永超 on 2017/5/15.
+//  Copyright © 2017年 g. All rights reserved.
+//
+
+import UIKit
+
+class WOWTaxExplainCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
